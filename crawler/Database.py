83edcd008875f1133dd87aaf55d11febccbd96bf
Davasny <davasny@gmail.com>
2018-09-01 15:23:59 +0200

    Remove MySQL

--- crawler/Database.py
+++ crawler/Database.py
@@ -95,35 +95,5 @@ engine = create_engine('mysql://{}:{}@{}:{}/{}?charset=utf8'.format(
 
 Base.metadata.create_all(engine)
 
-
-class MySQL:
-    def __init__(self):
-        server = "localhost"
-        user = "root"
-        password = ""
-        server_port = 3306
-        database = "otodom_crawler"
-
-        self.operating_database = pymysql.connect(host=server, user=user, password=password, database=database,
-                                             charset='utf8', port=server_port)
-
-        self.cur = self.operating_database.cursor()
-
-    def new_query(self, command):
-        if 'SELECT' in command:
-            self.cur.execute(command)
-            return self.cur.fetchall()
-        elif 'INSERT' in command:
-            self.cur.execute(command)
-            self.operating_database.commit()
-            return self.cur.lastrowid
-        elif 'UPDATE' in command:
-            self.cur.execute(command)
-            self.operating_database.commit()
-            return True
-        elif 'DELETE' in command:
-            self.cur.execute(command)
-            self.operating_database.commit()
-            return True
-        else:
-            return False
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
