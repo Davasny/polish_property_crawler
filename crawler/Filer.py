3eabc6cc61388d5b2c0d6925e6956ffe2c75eabe
Davasny <davasny@gmail.com>
2018-09-07 21:37:27 +0200

    Small class refactor

--- crawler/Filer.py
+++ crawler/Filer.py
@@ -1,4 +1,5 @@
-from os import listdir, remove
+import os
+from os import listdir, remove, path
 from os.path import isfile, join
 import logging
 
@@ -6,18 +7,18 @@ log = logging.getLogger("main")
 
 
 class Filer:
-    def __init__(self, download_path):
-        self._download_path = download_path
+    def __init__(self, dir_path):
+        self._dir_path = dir_path
 
     def get_all_files(self):
-        all_files = [f for f in listdir(self._download_path) if isfile(join(self._download_path, f))]
-        log.info("Files found:\t{}".format(len(all_files)))
+        all_files = [f for f in listdir(self._dir_path) if isfile(join(self._dir_path, f))]
+        log.debug("Files found:\t{}".format(len(all_files)))
         return all_files
 
     def empty_dir(self):
-        log.warning("Removing files from:\t{}".format(self._download_path))
+        log.warning("Removing files from:\t{}".format(self._dir_path))
         for file in self.get_all_files():
-            filename = "{}/{}".format(self._download_path, file)
+            filename = "{}/{}".format(self._dir_path, file)
             remove(filename)
             log.debug("Removed:\t{}".format(filename))
     def make_sure_if_path_exists(self):
